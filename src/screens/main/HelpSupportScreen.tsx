ea77839baad6e869c3e06d3ccac26388c94ecf4a
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-07 11:56:47 -0800

    setting up help and support screen

---
+++ src/screens/main/HelpSupportScreen.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useState, useRef } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Pressable,
+  PanResponder,
+  Animated,
+  ScrollView,
+  StatusBar
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { getAuthService } from "../../services";
+import { User, CategoryPreferences, MainStackParamList } from "../../types";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+type ProfileScreenNavigationProp = StackNavigationProp<MainStackParamList>;
+
+interface HelpSupportScreenProps {
+  navigation: any;
+  route?: {
+    params?: {
+      isInitialSetup?: boolean;
+    };
+  };
+}
+
+export const HelpSupportScreen: React.FC<HelpSupportScreenProps> = () => {
+
+    return(
+        <SafeAreaView>
+            
+        </SafeAreaView>
+    );
+};
