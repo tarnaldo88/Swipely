f5d4354777dbb5d25c4a723185c9735b542488fe
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-07 12:08:12 -0800

    adding some styling to help and priv screens

--- src/screens/main/Profile/HelpSupportScreen.tsx
+++ src/screens/main/Profile/HelpSupportScreen.tsx
@@ -35,8 +35,49 @@ export const HelpSupportScreen: React.FC<HelpSupportScreenProps> = () => {
   const [loading, setLoading] = useState(true);
 
     return(
-        <SafeAreaView>
-            
+        <SafeAreaView style={styles.container}>
+          <ScrollView style={styles.scrollView}>
+
+          </ScrollView>            
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#230234",
+  },
+  scrollView: {
+    flex: 1,
+  },
+  accountItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E9ECEF",
+    backgroundColor:'#fff'
+  },
+  accountLabel: {
+    flex: 1,
+    fontSize: 16,
+    color: "#212529",
+  },
+  header: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    backgroundColor: "#47006e",
+    borderBottomWidth: 1,
+    borderBottomColor: "#3a8004",
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorText: {
+    fontSize: 16,
+    color: "#dc3545",
+  },
+});
--- src/screens/main/Profile/PrivacySecurityScreen.tsx
+++ src/screens/main/Profile/PrivacySecurityScreen.tsx
@@ -31,10 +31,49 @@ interface PrivacySecurityScreenProps {
 
 export const PrivacySecurityScreen: React.FC<PrivacySecurityScreenProps> = () => {
     return(
-        <SafeAreaView>
-            <ScrollView>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scrollView}>
                 
             </ScrollView>
         </SafeAreaView>
     )
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#230234",
+  },
+  scrollView: {
+    flex: 1,
+  },
+  accountItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#E9ECEF",
+    backgroundColor:'#fff'
+  },
+  accountLabel: {
+    flex: 1,
+    fontSize: 16,
+    color: "#212529",
+  },
+  header: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    backgroundColor: "#47006e",
+    borderBottomWidth: 1,
+    borderBottomColor: "#3a8004",
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  errorText: {
+    fontSize: 16,
+    color: "#dc3545",
+  },
+});
