ede97b1d350c28f031fab4e57ee3d72a3213ce41
tarnaldo88 <tarnaldo88@gmail.com>
2025-11-11 11:47:30 -0800

    Making a mock terms of service to load into help & support.

---
+++ src/screens/main/Profile/TermsOfService.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
+import Accordion from "react-native-collapsible/Accordion";
+
+const MOCKTOS = [
+  
+];
+
